a8bd5e0dfb71bda9c33295dc7d7f2f7e2278faa7
Chuck R <chuck.the.pc.guy+github@gmail.com>
2019-10-14 01:02:10 -0500

    RunESO.sh: Add update and self-update method

--- extra/RunESO.sh
+++ extra/RunESO.sh
@@ -15,13 +15,15 @@ game_dir="$eso_path/pfx/drive_c/Program Files (x86)/Zenimax Online"
 
 function printusage()
 {
-  echo "Usage: $0 [launcher|game|install [Install_ESO.exe|--prefix-only]]"
+  echo "Usage: $0 [launcher|game|update|install [Install_ESO.exe|--prefix-only]]"
   echo "  This script defaults to running the launcher."
   echo
   echo "  Options:"
   echo -e "    \e[1mlauncher\e[0m:  Run the launcher"
   echo
   echo -e "    \e[1mgame\e[0m:      Run the game"
+  echo
+  echo -e "    \e[1mupdate\e[0m:   Update proton-strftime"
   echo
   echo -e "    \e[1minstall [Install_ESO.exe]\e[0m:"
   echo "               Automated install script"
@@ -49,7 +51,7 @@ arg="launcher"
 
 if [ ! -z "$1" ]
 then
-  if [ "$1" != "launcher" ] && [ "$1" != "game" ] && [ "$1" != "install" ]
+  if [ "$1" != "launcher" ] && [ "$1" != "game" ] && [ "$1" != "install" ] && [ "$1" != "update" ]
   then
     printusage
     exit
@@ -70,10 +72,10 @@ then
 elif [ "$arg" == "game" ]
 then
   eval env $extra_proton_env $eso_path/proton/proton waitforexitandrun "\"$game_dir/The Elder Scrolls Online/game/client/eso64.exe\""
-elif [ "$arg" == "install" ]
+elif [ "$arg" == "install" ] || [ "$arg" == "update" ]
 then
   prefixonly=0
-  if [ ! -z "$2" ]
+  if [ "$arg" == "install" ] && [ ! -z "$2" ]
   then
     if [ "$2" == "--prefix-only" ]
     then
@@ -98,9 +100,18 @@ then
     exit
   fi
 
-  echo -n "Creating ESO directory..."
-  mkdir -p "$eso_path"
-  echo " Done."
+  if [ ! -d "$eso_path" ]
+  then
+    echo -n "Creating ESO Proton directory..."
+    mkdir -p "$eso_path"
+    echo " Done."
+  else
+    if [ "$arg" == "install" ]
+    then
+      echo -e "\e[1mError: ESO Proton directory '$eso_path' already exists. Refusing to install into existing directory.\e[0m"
+      exit
+    fi
+  fi
 
   archive="proton-strftime.tar.xz"
   archiver="$(which tar) -C \"$eso_path\" -xJf \"$eso_path/$archive\""
@@ -114,18 +125,38 @@ then
       echo -e "\e[1mError: wget or curl not installed, cannot fetch the archive. Please intall wget or curl.\e[0m"
       exit
     fi
-    fetcher="$fetcher -LR -o \"$eso_path/$archive\""
+    fetcher="$fetcher -LR --progress-bar -o \"$eso_path/$archive\""
     if [ -f "$eso_path/$archive" ]
     then
-      fetcher="$fetcher -z \"$eso_path/$archive\""
+      fetcher="$fetcher --progress-bar -z \"$eso_path/$archive\""
     fi
   else
-    fetcher="$fetcher -P \"$eso_path\" -N"
+    fetcher="$fetcher -q --show-progress -P \"$eso_path\" -N"
   fi
 
-  fetcher="$fetcher https://github.com/chuck-r/Proton/releases/latest/download/$archive"
+  echo "Checking for script updates..."
+  oldfiletime=$(stat -c %Y "$0")
+  eval $fetcher https://raw.githubusercontent.com/chuck-r/proton-strftime/extra/extra/RunESO.sh
+
+  if [ $? -ne 0 ]
+  then
+    echo -e "\e[1mWarning: Could not fetch script update. This could mean that it has moved. Re-download a copy from https://github.com/chuck-r/proton-strftime.\e[0m"
+  else
+    newfiletime=$(stat -c %Y "$eso_path/RunESO.sh")
+    if [ $oldfiletime -lt $newfiletime ]
+    then
+      echo -e "\e[1mUpdating script and re-executing.\e[0m"
+      cp -a "$eso_path/RunESO.sh" "$0"
+      chmod +x "$0"
+      eval $0 $@
+      exit
+    fi
+  fi
+
+  oldfiletime=$(stat -c %Y $eso_path/proton-strftime.tar.xz 2>/dev/null)
+
   echo "Downloading $archive..."
-  eval $fetcher
+  eval "$fetcher https://github.com/chuck-r/proton-strftime/releases/latest/download/$archive"
 
   if [ $? -ne 0 ]
   then
@@ -134,31 +165,48 @@ then
     exit
   fi
 
-  echo "Extracting $eso_path/$archive..."
+  if [ ! -z "$oldfiletime" ] && [ $(stat -c %Y $eso_path/proton-strftime.tar.xz) -eq $oldfiletime ]
+  then
+    if [ "$arg" == "update" ]
+    then
+      echo "No update required."
+      exit
+    fi
+  fi
+
+  echo -n "Extracting $eso_path/$archive..."
   eval $archiver
 
   if [ $? -ne 0 ]
   then
-    echo -e "\e[1mError: Could not extract $eso_path/$archive\e[0m"
+    echo -e "\n\e[1mError: Could not extract $eso_path/$archive\e[0m"
     rm "$eso_path/$archive"
     exit
   fi
+  echo " Done."
 
   if [ -L "$eso_path/proton" ]
   then
+    oldproton=$(readlink -f "$eso_path/proton")
     echo -n "Removing old symlink..."
     rm "$eso_path/proton"
+    echo "Done."
+    echo -n "Removing old Proton directory..."
+    rm -R "$oldproton"
     echo " Done."
   fi
   echo -n "Creating $eso_path/proton symlink..."
-  ln -s $(ls -dt --group-directories-first "$eso_path"/proton-* | head -n 1) "$eso_path/proton"
+  ln -s $(ls -dt --group-directories-first "$eso_path"/proton-*-strftime | head -n 1) "$eso_path/proton"
   echo " Done."
 
-  echo -n "Copying Proton prefix to $eso_path..."
-  cp -R "$eso_path/proton/dist/share/default_pfx" "$eso_path/pfx"
-  echo " Done."
+  if [ "$arg" == "install" ]
+  then
+    echo -n "Copying Proton prefix to $eso_path..."
+    cp -R "$eso_path/proton/dist/share/default_pfx" "$eso_path/pfx"
+    echo " Done."
+  fi
 
-  if [ $prefixonly -eq 0 ]
+  if [ $prefixonly -eq 0 ] && [ "$arg" == "install" ]
   then
     echo "Running ESO installer..."
     STEAM_COMPAT_DATA_PATH="$eso_path" "$eso_path/proton/proton" waitforexitandrun "$installer_path"
